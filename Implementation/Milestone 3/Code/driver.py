5f23c5ae0044a5d53531c004a26de91e21674d04
zacMadison <149635415+zacMadison@users.noreply.github.com>
2025-04-02 10:13:15 -0500

    Added milestone three code

---
+++ Implementation/Milestone 3/Code/driver.py
@@ -0,0 +1,124 @@
+# Best Group/Group Python
+# All code surrounding pygame heavily references pygame documentation
+# Milestone One: "Snake" that can be controlled by the player
+import pygame
+import time
+from python import Python
+from food import Food
+
+
+def main():
+    running = True
+    # initializes pygame, a clock (for frame rate/anything else we might need it for later, and the snake object
+    screen, clock, snake, food = initialize()
+    # used to ensure movement only occurs every 100 ms (subject to change)
+    movement_timer = time.time()
+
+    # This loop runs through different functions until the user closes the window
+    while running:
+        # sets frame rate
+        clock.tick(60)
+        # calls render function to render screen
+        render(screen, snake, food)
+        # checks input
+        snake = key_input(snake)
+        # border function set up where if the snake head touches it, the game quits running
+        border(snake)
+        if snake.get_curr_direction() == "stop":
+            running = False
+        # calls the function from the python class that changes the position of the snakes head
+        # if statement triggers every 100 ms and resets timer
+        if movement_timer - time.time() < -0.1:
+            snake.change_head_pos()
+            movement_timer = time.time()
+        # Allows "X" button on window to be pressed
+
+        # Need a coordinate system so that fruit placement can be randomized
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    return
+
+
+# initializes pygame: referenced https://www.pygame.org/docs/, also referenced for rendering
+def initialize():
+    pygame.init()
+    snake = Python()
+    display = pygame.display.set_mode(size=(720, 720))
+    clock = pygame.time.Clock()
+    food = Food()
+    return display, clock, snake, food
+
+
+def render(screen, snake, food):
+    # var for rectangle size
+    rect_size = 20
+    # var for food size
+    food_size = 10
+    # list stores rectangles to check for collision
+    snakes_squares = []
+    # This fills the background, so the previous image does not stay on screen
+    screen.fill("white")
+    # update score
+    pygame.display.set_caption("Score: " + str(snake.get_curr_score()))
+    # render snake here
+    for length in range(len(snake.get_pos_list())):
+        # uses draw.rect() which is formatted  as (display being drawn on, color,
+        #                                         rect(x position, y position, width, length)
+        snakes_squares.append(pygame.Rect(snake.get_pos_list()[length].x, snake.get_pos_list()[length].y,
+                                          rect_size, rect_size))
+        pygame.draw.rect(screen, "green", rect=(snakes_squares[length]))
+
+    apple = pygame.draw.circle(screen, "red", (food.get_curr_pos().x, food.get_curr_pos().y), food_size)
+    apple_collision(snakes_squares, apple, snake, food)
+    # renders display, end of function
+    pygame.display.flip()
+
+
+def key_input(snake):
+    # this creates a list of booleans depending on what keys are being pressed
+    keys = pygame.key.get_pressed()
+    # check keys being pressed, and changes current direction depending on the key being pressed
+    # pygame.K_CHARACTER references the point in the list referencing that character
+    if keys[pygame.K_UP] or keys[pygame.K_w]:
+        snake.set_curr_direction("up")
+
+    elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        snake.set_curr_direction("down")
+
+    elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        snake.set_curr_direction("right")
+
+    elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        snake.set_curr_direction("left")
+
+    return snake
+
+
+def apple_collision(snake_rects, apple_circle, snake, food):
+    if snake_rects[0].collidepoint(food.get_curr_pos()):
+        forbidden_x = snake.get_x_coordinates()
+        forbidden_y = snake.get_y_coordinates()
+        snake.set_curr_score(snake.get_curr_score() + 1)
+        food.position_change(forbidden_x, forbidden_y)
+        # this will also update snake length later
+
+
+def border(snake):
+    if snake.get_pos_list()[0][0] <= 0 or snake.get_pos_list()[0][0] >= 720:
+        snake.set_curr_direction("stop")
+    elif snake.get_pos_list()[0][1] <= 0 or snake.get_pos_list()[0][1] >= 720:
+        snake.set_curr_direction("stop")
+
+    return snake
+
+# Need a function to generate the head?
+# sure, it can be called from the render function
+'''
+def adam_head():
+    # head = pygame.image.load("spanieram.jpg").convert_alpha()
+'''
+
+if __name__ == "__main__":
+    main()
---
+++ Implementation/Milestone 3/Code/python.py
@@ -0,0 +1,93 @@
+# Best Group/Group Python
+import pygame
+
+
+class Python:
+    # remember to create a getter and setter if you create a new var (and also to set it in __init__)
+    __length = -1
+    # pygame uses format K_DIRECTION, so that's what I'm using
+    __curr_direction = "ERR"
+    __curr_score = -1
+    __head_pos = -1
+    # This list will contain a list of vectors to draw from using pygame.math.Vector2(), which stores an x and y value
+    # individual X or Y value can be called with [variable].x or [variable].y
+    __pos_list = [None]
+
+    # initializes class once created
+    def __init__(self):
+        self.set_length(1)
+        self.set_curr_direction("none")
+        self.set_curr_score(0)
+        self.set_pos_list([pygame.math.Vector2(480, 360)])
+        self.set_head_pos(pygame.math.Vector2(480, 360))
+
+    # helpers
+
+    # changes position of head depending on the state of __curr_direction
+    def change_head_pos(self):
+        print(self.get_curr_direction())
+        match self.get_curr_direction():
+            case "up":
+                self.set_head_pos(pygame.math.Vector2(self.get_head_pos().x, self.get_head_pos().y - 20))
+            case "down":
+                self.set_head_pos(pygame.math.Vector2(self.get_head_pos().x, self.get_head_pos().y + 20))
+            case "right":
+                self.set_head_pos(pygame.math.Vector2(self.get_head_pos().x + 20, self.get_head_pos().y))
+            case "left":
+                self.set_head_pos(pygame.math.Vector2(self.get_head_pos().x - 20, self.get_head_pos().y))
+        # updates variable
+        lst_change = self.get_pos_list()
+        lst_change[0] = self.get_head_pos()
+        self.set_pos_list(lst_change)
+
+    # returns x coordinates of get_pos_list() this is used in apple_collision for forbidden coordinates
+    def get_x_coordinates(self):
+        x = []
+        for length in self.get_pos_list():
+            x.append(length.x)
+        return x
+
+    # same thing but for y
+    def get_y_coordinates(self):
+        y = []
+        for length in self.get_pos_list():
+            y.append(length.y)
+        return y
+
+    # getters
+    def get_length(self):
+        return self.__length
+
+    def get_curr_direction(self):
+        return self.__curr_direction
+
+    def get_curr_score(self):
+        return self.__curr_score
+
+    def get_pos_list(self):
+        return self.__pos_list
+
+    def get_head_pos(self):
+        return self.__head_pos
+
+    # setters
+    def set_length(self, length):
+        self.__length = length
+
+    def set_curr_direction(self, direction):
+        self.__curr_direction = direction
+
+    def set_curr_score(self, score):
+        self.__curr_score = score
+
+    def set_pos_list(self, positions):
+        self.__pos_list = positions
+
+    def set_head_pos(self, head_position):
+        self.__head_pos = head_position
+
+    # __str__
+    def __str__(self):
+        tempstr = ("Length: " + str(self.get_length()) + "\nDirection: " + str(self.get_curr_direction()) +
+                   "\n Score:" + str(self.get_curr_score()) + "\nPosition list: " + str(self.get_pos_list()))
+        return tempstr
